2bfc2efe8bed965bf43f9b85cd90a71fa441efad
GennaroP <GennaroP@mvpsi.com>
2017-07-11 12:47:42 -0400

    Incorrect Table
    Made changes to script to reflect correct table

--- SearchAndReplace.sql
+++ SearchAndReplace.sql
@@ -1,22 +1,22 @@
--- Old Value that we are replacing
-DECLARE @findString NVARCHAR(MAX) = 'MyOldValue'
+--- Old Value that we are replacing
+DECLARE @findString NVARCHAR(MAX) = 'OldValue'
 
 -- Value to replace with
-DECLARE @newValue NVARCHAR(MAX) = 'MyNewValue'
+DECLARE @newValue NVARCHAR(MAX) = 'NewValue'
 
 /*
 ** This lists the Job that will be updated and what it's source will be after the update
  */
-SELECT j.job_name,  t.source as 'Current Source', REPLACE(cast(t.Source as nvarchar(max)),@findString,@newValue) as 'New Source'
-FROM [JAMS].[dbo].[JobText] as t
-	LEFT OUTER JOIN [JAMS].[dbo].[Job] as j on t.job_id = j.job_id
-WHERE PATINDEX('%' + @findString + '%', [source]) > 0
-  
-  
+
+SELECT job_name, job_id, source
+  from [JAMS].[dbo].[job]
+  where PATINDEX('%' + @findstring + '%', [source]) > 0
+
 /*
 ** This does the actual replace. Note: Keep this commented out until you've run the above
 **    portion and verified the results are what you would expect
  */
---UPDATE [JAMS].[dbo].[JobText]
+
+--UPDATE [JAMS].[dbo].[Job]
 --SET [source] = REPLACE(cast(Source as nvarchar(max)),@findString,@newValue)
 --WHERE PATINDEX('%' + @findString + '%', [source]) > 0
